49d4fba6b0a742d4a9a83d8bd3135a07391f2e38
Victor de Carli <vonorvictor@gmail.com>
2025-04-09 13:15:30 -0400

    [fix] Oculta alguns links protegidos para ser visivel apenas para o admin

--- frontend/src/components/AdminNavbar.jsx
+++ frontend/src/components/AdminNavbar.jsx
@@ -3,7 +3,6 @@ import { logout } from '../utils/auth';
 
 export default function AdminNavbar() {
   const navigate = useNavigate();
-
   const handleLogout = () => {
     logout();
     navigate('/login');
@@ -11,7 +10,7 @@ export default function AdminNavbar() {
 
   return (
     <nav className="bg-white shadow flex justify-between items-center px-6 py-3 mb-6">
-      <div className="text-blue-700 font-bold text-lg">Painel Admin</div>
+      <div className="text-blue-700 font-bold text-lg"> {localStorage.getItem('role') === 'admin' ? 'Admin' : 'User'}</div>
       <div className="flex items-center gap-4 text-sm">
         <Link to="/products" className="hover:underline">
           Produtos
@@ -19,12 +18,16 @@ export default function AdminNavbar() {
         <Link to="/orders" className="hover:underline">
           Meus Pedidos
         </Link>
-        <Link to="/admin-orders" className="hover:underline">
-          Pedidos (Admin)
-        </Link>
-        <Link to="/products/new" className="hover:underline">
-          + Novo Produto
-        </Link>
+        {localStorage.getItem('role') === 'admin' && (
+          <Link to="/admin-orders" className="hover:underline">
+            Pedidos (Admin)
+          </Link>
+         )}
+        {localStorage.getItem('role') === 'admin' && (
+          <Link to="/products/new" className="hover:underline">
+            + Novo Produto
+          </Link>
+        )}
         <button
           onClick={handleLogout}
           className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 ml-2"
